f550c0f4720810ee44dd0737ad7dbeebfc2e0189
no-chili <18875854140@163.com>
2023-01-03 17:40:19 +0800

    ts jest

---
+++ src/browser/index.ts
@@ -0,0 +1,28 @@
+/*
+ * @Author: no-chili 18875854140@163.com
+ * @LastEditTime: 2023-01-03 15:45:04
+ * @Description: 浏览器环境
+ */
+const UA = navigator.userAgent
+
+// 微信
+
+// Android
+
+// iOS
+
+// PC
+
+// 火狐
+
+// Edge
+
+// Mac
+
+// Window
+
+// Safari
+
+// iPhone
+
+// iPad
